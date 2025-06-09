77b434abd67ed4d85c11fa5a979c7d9f4d4d6fe6
John Andrews <torrental@gmail.com>
2025-06-09 15:11:34 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import { R2Explorer } from "r2-explorer";
 
 export default R2Explorer({
   // Set to false to allow users to upload files
-  readonly: true,
-
+  readonly: false,
   // Learn more how to secure your R2 Explorer instance:
   // https://r2explorer.com/getting-started/security/
   // cfAccessTeamName: "my-team-name",
+  basicAuth: [{
+    username: 'john',
+    password: 'Ioi8(Y09hw)&%*776'
+  },{
+    username: 'john43',
+    password: 'ppnH*08h209hSaarr@$5'
+  }]
 });
